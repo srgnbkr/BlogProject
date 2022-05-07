d95a80c19d31d3e3f33dec31460f88cafc79aac3
Sergen Bakır <53319517+srgnbkr@users.noreply.github.com>
2022-05-07 21:11:27 +0300

    Article Index Added

---
+++ BlogProject.MvcUI/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,46 @@
+﻿using BlogProject.Services.Abstract;
+using BlogProject.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlogProject.MvcUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticleController : Controller
+    {
+        #region Variables
+        private readonly IArticleService _articleService;
+
+
+        #endregion
+
+        #region Constructor
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        #endregion
+
+
+        #region GetAll Methods
+        [HttpGet]        
+        public async Task<IActionResult> Index()
+        {
+            var result = await _articleService.GetAllByNonDeletedAsync();
+            if (result.ResultStatus == ResultStatus.Success)
+                return View(result.Data);
+            return NotFound();
+            
+            
+        }
+        #endregion
+
+        #region AddMethods
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
